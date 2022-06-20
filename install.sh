7c9023915e3c0e0ebe1f1651590b498de5c4e494
Jesus-JAR <93333212+Jesus-JAR@users.noreply.github.com>
2022-06-20 09:33:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 # instalar php 8.1 y activar
 echo instalando php 8.1 y activandolo
-sudo a2dismod php8.0 -y
-sudo apt purge php8.0 -y
-sudo apt remove php libapache2-mod-php
 sudo apt update && sudo apt upgrade -y
 sudo apt install php8.1 -y
 sudo a2dismod php8.0
